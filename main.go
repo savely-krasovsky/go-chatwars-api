ee668c5e32ea640a1a75cb5ee8b479fb0cdf9d7c
Savely Krasovsky <savely@krasovsky.me>
2018-06-07 01:11:18 +0300

    Typo fix

--- main.go
+++ main.go
@@ -268,7 +268,7 @@ func (c *Client) makeRequest(req []byte) (err error) {
 		},
 	)
 	// If channel closed
-	if err != nil && err.(amqp.Error).Code == 504 {
+	if err != nil && err.(*amqp.Error).Code == 504 {
 		// Ensure we close it
 		err := c.channelForPublish.Close()
 		if err != nil {
